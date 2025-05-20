90829fedda20177bdacee5d14b593e159672310c
Mateus Boche <mateussenaisrs@gmail.com>
2025-05-20 10:28:44 -0300

    feat: adicao no beckend a funcao listar agendamentos para as profissionais dew saude

--- PFC_2025_Cods/bocheBeckend/models/doacaoModel.js
+++ PFC_2025_Cods/bocheBeckend/models/doacaoModel.js
@@ -1,14 +1,33 @@
 const pool = require('../config/db');
 
-exports.buscarTodos = async () => {
-  const result = await pool.query('SELECT * FROM doacao ORDER BY data_agendada ASC');
-  return result.rows;
+const Doacao = {
+
+  async buscarTodos() {
+    const query = `
+      SELECT
+        d.id,
+        d.data_doacao,
+        d.status,
+        d.quantidade_ml,
+        u.nome AS nome_doadora
+      FROM doacao d
+      JOIN usuario u ON d.id_usuario = u.id
+      ORDER BY d.data_doacao DESC
+    `;
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async atualizarStatus(id, novoStatus) {
+    const query = 'UPDATE doacoes SET status = $1 WHERE id = $2';
+    await pool.query(query, [novoStatus, id]);
+  },
+
+  async reagendar(id, novaData) {
+    const query = 'UPDATE doacoes SET data_agendamento = $1, status = $2 WHERE id = $3';
+    await pool.query(query, [novaData, 'reagendado', id]);
+  }
+
 };
 
-exports.atualizarStatus = async (id, status) => {
-  await pool.query('UPDATE doacao SET status = $1 WHERE id = $2', [status, id]);
-};
-
-exports.reagendar = async (id, novaData) => {
-  await pool.query('UPDATE doacao SET data_agendada = $1, status = $2 WHERE id = $3', [novaData, 'reagendado', id]);
-};
+module.exports = Doacao;
